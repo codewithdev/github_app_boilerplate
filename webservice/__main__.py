ebedc4cc22a8db8f58d801ea6a4adf09ba8b39dd
Dev Prakash Sharma <44609609+codewithdev@users.noreply.github.com>
2020-10-06 12:10:05 +0530

    Update __main__.py

--- webservice/__main__.py
+++ webservice/__main__.py
@@ -51,6 +51,32 @@ async def repo_installation_added(event, gh, *args, **kwargs):
     installation_id = event.data["installation"]["id"]
     pass
 
+@router.register("pull_request", action="opened")
+async def pr_opened(event, gh, *args, **kwargs):
+    installation_id = event.data["installation"]["id"]
+    installation_access_token = await utils.get_installation_access_token(
+        gh, installation_id
+    )
+
+    url = event.data["pull_request"]["comments_url"]
+    author_association = event.data["pull_request"]["author_association"]
+    author = event.data["pull_request"]["user"]["login"]
+    if author_association == "NONE":
+        await gh.post(
+            url,
+            data={"body": f"Thanks for the PR, @{author}. Thanks for your first contribution!"},
+            oauth_token=installation_access_token["token"],
+        )
+    else:
+        await gh.post(
+            url,
+            data={
+                "body": f"Welcome back @{author}, you're a {author_association} of this project."
+            },
+            oauth_token=installation_access_token["token"],
+        )
+        
+        
 
 if __name__ == "__main__":  # pragma: no cover
     app = web.Application()
